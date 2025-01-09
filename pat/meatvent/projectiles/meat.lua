1a2078696151b55a9e8ea5fa5a9d7b72dd3b7e5b
patman <27874300+patmanf@users.noreply.github.com>
2025-01-08 22:19:45 -0600

    redo meat.lua . still for no good reason

--- pat/meatvent/projectiles/meat.lua
+++ pat/meatvent/projectiles/meat.lua
@@ -1,26 +1,25 @@
-require "/scripts/vec2.lua"
-
 function init()
-	rotationSpeed = config.getParameter("rotationSpeed", 20)
-	mcontroller.setRotation(math.random() * math.pi * 2)
-	
-	bounces = config.getParameter("maxBounces")
-	stickSettings = config.getParameter("stickSettings")
+  self.rotationSpeed = config.getParameter("rotationSpeed", 20)
+  mcontroller.setRotation(math.random() * math.pi * 2)
+  
+  self.bounces = config.getParameter("maxBounces")
 end
 
 function update(dt)
-	local vel = mcontroller.velocity()
-	local dir = vel[1] > 0 and 1 or -1
-	
-	local rot = (vec2.mag(vel) / 180 * math.pi) * -dir * dt * rotationSpeed
-	mcontroller.setRotation(mcontroller.rotation() + rot)
+  local vel = mcontroller.velocity()
+  local dir = vel[1] > 0 and 1 or -1
+  
+  local mag = math.sqrt(vel[1] * vel[1] + vel[2] * vel[2])
+  local rot = (mag / 180 * math.pi) * -dir * dt * self.rotationSpeed
+  mcontroller.setRotation(mcontroller.rotation() + rot)
 end
 
 function bounce()
-	if bounces > 0 then
-		bounces = bounces - 1
-		if bounces == 0 then
-			mcontroller.applyParameters(stickSettings)
-		end
-	end
+  if self.bounces == 0 then return end
+
+  self.bounces = self.bounces - 1
+  if self.bounces == 0 then
+    local params = config.getParameter("stickSettings")
+    mcontroller.applyParameters(params)
+  end
 end
