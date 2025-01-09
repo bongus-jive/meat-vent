3b53d47dfea629ab00b1dfaf0d09117c9d4fb149
patman <27874300+patmanf@users.noreply.github.com>
2025-01-08 21:21:57 -0600

    rewrite vent.lua for no reason

--- pat/meatvent/vent.lua
+++ pat/meatvent/vent.lua
@@ -1,47 +1,69 @@
-require "/scripts/util.lua"
-require "/scripts/vec2.lua"
-require "/scripts/rect.lua"
-
 function init()
-	meat = config.getParameter("meat")
-	timer = 0.1
-	parameters = {}
-	center = rect.center(object.boundBox())
+  self.projectile = config.getParameter("meatProjectile")
+  self.projectile.position = object.toAbsolutePosition(self.projectile.offset)
+
+  self.pitchRange = config.getParameter("soundPitchRange")
+
+  self.timeRange = config.getParameter("timeRange")
+  resetTimer()
   
-  onInputNodeChange({node = 0, level = object.getInputNodeLevel(0)})
+  onNodeConnectionChange()
 end
 
 function update(dt)
-	timer = math.max(0, timer - dt)
-	if timer == 0 then
-		timer = util.randomInRange(meat.time)
-		
-		parameters.speed = util.randomInRange(meat.speed)
-		parameters.processing = "."..math.random(1, meat.variants)
-		
-		local flips = math.random(0, 3)
-		if flips ~= 0 then
-			local f = "?flip"
-			if flips > 1 then f = f.."x" end
-			if flips < 3 then f = f.."y" end
-			parameters.processing = parameters.processing..f
-		end
-		
-		world.spawnProjectile(meat.projectile, center, nil, vec2.rotate({-1, 0}, math.random() * math.pi), nil, parameters)
-		
-		animator.setSoundPitch("meat", util.randomInRange(meat.soundPitch))
-		animator.playSound("meat")
-		animator.burstParticleEmitter("blood")
-	end
+  if not self.meatEnabled then return end
+
+  self.timer = self.timer - dt
+  if self.timer <= 0 then
+    resetTimer()
+    spawnMeat()
+  end
+end
+
+function resetTimer()
+  self.timer = randomInRange(self.timeRange)
+end
+
+function spawnMeat()
+  world.spawnProjectile(self.projectile.type, self.projectile.position, nil, meatVector(), nil, meatParams())
+    
+  animator.setSoundPitch("meat", randomInRange(self.pitchRange))
+  animator.playSound("meat")
+  animator.burstParticleEmitter("blood")
+end
+
+function meatParams()
+  local params = {}
+  params.speed = randomInRange(self.projectile.speedRange)
+  params.processing = "." .. math.random(self.projectile.variants)
+
+  local rand = math.random(0, 3)
+  if rand > 0 then
+    local flip = "?flip"
+    if rand > 1 then flip = flip .. "x" end
+    if rand < 3 then flip = flip .. "y" end
+    params.processing = params.processing .. flip
+  end
+
+  return params
+end
+
+function meatVector()
+  local angle = math.random() * -math.pi
+  return {math.cos(angle), math.sin(angle)}
+end
+
+function randomInRange(range)
+  return range[1] + (math.random() * (range[2] - range[1]))
 end
 
 function onInputNodeChange(args)
   if args.node == 0 then
     local connected = object.isInputNodeConnected(0)
-    if not connected then args.level = true end
+    if not object.isInputNodeConnected(0) then args.level = true end
     
     animator.setParticleEmitterActive("drip", args.level)
-    script.setUpdateDelta(args.level and 1 or 0)
+    self.meatEnabled = args.level
   end
 end
 
